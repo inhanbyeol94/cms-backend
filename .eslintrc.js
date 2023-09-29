c2135f34aa58d3896898672ab22a1571c830f314
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-30 01:07:39 +0900

    [Style] printWidth 150, tab 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': ['error', { printWidth: 150 }], // 줄바꿈 150 적용
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
